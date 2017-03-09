2a9f6788e24f0ce988a1e3dba99d7e65e7939159
NeverBehave <luoxinhao77@gmail.com>
2017-03-09 12:52:33 +0800

    try fixing

--- api/index.php
+++ api/index.php
@@ -1,22 +1,22 @@
 <?php
 error_reporting(0);
-if(isset($_GET['addr']) && isset($_SERVER['HTTP_REFERER'])){
-    $url = urldecode($_GET['addr']);
-    $referer = parse_url($_SERVER['HTTP_REFERER']);
-    if(preg_match("/^http/",$url) && $referer['host']=='status.hfi.com'){//防盗链验证
-        $opts = array('http'=>array(
-            'method'=>'GET',
-            'timeout'=>10
-        ));
-        $result = file_get_contents('http://stats.uptimerobot.com/'.$url,FALSE,stream_context_create($opts));
-        if($result){
-            echo $result;
-        }else{
-            header("HTTP/1.1 404 Not Found");
-        }
-    }else{
-        header("HTTP/1.1 400 Bad Request");
-    }
-}else{
-    header("HTTP/1.1 403 Forbidden");
+if (isset($_GET['addr']) && isset($_SERVER['HTTP_REFERER'])) {
+	$url = urldecode($_GET['addr']);
+	$referer = parse_url($_SERVER['HTTP_REFERER']);
+	if (true) {//防盗链验证 $referer['host'] == ''
+		$opts = ['http' => [
+			'method'  => 'GET',
+			'timeout' => 10,
+		]];
+		$result = file_get_contents('http://stats.uptimerobot.com/' . $url, false, stream_context_create($opts));
+		if ($result) {
+			echo $result;
+		} else {
+			header("HTTP/1.1 404 Not Found");
+		}
+	} else {
+		header("HTTP/1.1 400 Bad Request");
+	}
+} else {
+	header("HTTP/1.1 403 Forbidden");
 }
