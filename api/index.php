05f23d71fc9cd1f35f421b24e2b827b4950dc2bf
NeverBehave <luoxinhao77@gmail.com>
2017-03-10 10:51:42 +0800

    fix file

--- api/index.php
+++ api/index.php
@@ -31,7 +31,7 @@ if (isset($_GET['function'])) {
 
 function readMain($function, $url)
 {
-	$home = '/data/';
+	$home = '/data/htdocs/uptimebot-app.smartgslb.com/data';
 	if ((!file($home . $function . 'time')) || (time() - file($home . $function . 'time')[0] >= 180)) {
 		$timefile = fopen($home . $function . 'time', "w") or die('error while open file');
 		$time = time();
